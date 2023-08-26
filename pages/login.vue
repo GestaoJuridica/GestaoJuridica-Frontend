274b8d7ec534f0c00f1d659d3d459075e73778ef
GustavoFerrazRocha <gustavoguusfr@gmail.com>
2023-08-26 12:41:02 -0300

    style: ajustado responsividade da logo

--- pages/login.vue
+++ pages/login.vue
@@ -2,10 +2,12 @@
   <div class="login">
     <header class="login__header">
       <img
+        class="login__background"
         src="../public/imgs/loginImage.svg"
         alt="Imagem de um forum com a marreta do juiz"
       />
       <img
+        class="login__logo"
         src="https://www.vectorlogo.zone/logos/google/google-icon.svg"
         alt="Logo do nosso app"
       />
@@ -16,11 +18,26 @@
 
 <script setup lang="ts"></script>
 
-<style scoped>
+<style scoped lang="scss">
 .login {
   display: flex;
   flex-direction: column;
   height: 100vh;
   justify-content: space-between;
+
+  &__header {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__background {
+    opacity: 0.1;
+  }
+
+  &__logo {
+    position: absolute;
+  }
 }
 </style>
