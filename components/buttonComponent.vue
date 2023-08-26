098e99b6dd2d88c7b28251421c77ec57f5802b47
GustavoFerrazRocha <gustavoguusfr@gmail.com>
2023-08-26 01:02:24 -0300

    criado componente de botao

---
+++ components/buttonComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>{{ props.text }}</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: { type: String, required: true },
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  width: 50%;
+  background-color: #007ef2;
+  color: #fff;
+  border-radius: 10px;
+  height: 40px;
+}
+</style>
